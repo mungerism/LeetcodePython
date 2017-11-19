eddcd31366dba76f27b03e6c8e61c9d29aebcb23
Utopia <izentopia@gmail.com>
2017-11-19 18:52:57 +0800

    优化合并

--- 132.palindrome_partitioning_II.py
+++ 132.palindrome_partitioning_II.py
@@ -8,7 +8,6 @@ Return the minimum cuts needed for a palindrome partitioning of s.
 For example, given s = "aab",
 Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
 """
-
 class Solution:
     def minCut(self, s):
         """
@@ -18,8 +17,7 @@ class Solution:
         optimal_matrix = [x for x in range(len(s))]
 
         for i in range(1, len(s)):
-            optimal_matrix[i] = optimal_matrix[i - 1] + 1
-            for j in range(i):
+            for j in range(i + 1):
                 sub_s = s[j : i + 1]
                 if sub_s == sub_s[ : :-1]:
                     if j == 0:
@@ -28,9 +26,9 @@ class Solution:
                         optimal_matrix[i] = min(optimal_matrix[j - 1] + 1, optimal_matrix[i])
 
         return optimal_matrix[len(s) - 1]
-                
+
 
 if __name__ == '__main__':
     solution = Solution()
-    print (solution.minCut("cdd"))
+    solution.minCut("aab")
 
