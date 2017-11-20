eef5ff63c708131fa48b24163a5d354123d8a924
Utopia <izentopia@gmail.com>
2017-11-20 23:35:25 +0800

    增加宝宝写的

--- 120.Triangle.py
+++ 120.Triangle.py
@@ -46,3 +46,24 @@ if __name__ == '__main__':
             ]
     solution = Solution()
     print(solution.minimumTotal(triangle))
+
+"""宝宝写的
+class Solution:
+    def minimumTotal(self, triangle):
+     
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = triangle[i][j]+min(triangle[i+1][j],triangle[i+1][j+1])
+        return (triangle[0][0])
+
+if __name__ == '__main__':
+    triangle = [
+                 [2],
+                [3,4],
+               [6,5,7],
+              [4,1,8,3]
+            ]
+    solution = Solution()
+    solution.minimumTotal(triangle)
+
+"""
