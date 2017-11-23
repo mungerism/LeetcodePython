2c5af38798126f3e75970469ca41fccd58da27ed
Utopia <izentopia@gmail.com>
2017-11-23 11:06:22 +0800

    变量名修改

--- 51.N-Queens.py
+++ 51.N-Queens.py
@@ -25,15 +25,14 @@ There exist two distinct solutions to the 4-queens puzzle:
 
 class Solution:
 
-    def is_valid(self, column, row, queue_record):
-        r_left = row
-        r_right = row
-        for c in range(column, -1, -1):
-            if queue_record[c] == row or queue_record[c] == r_left or queue_record[c] == r_right:
+    def is_valid(self, row, column, queue_record):
+        c_left = column
+        c_right = column
+        for r in range(row, -1, -1):
+            if queue_record[r] == column or queue_record[r] == c_left or queue_record[r] == c_right:
                 return False
-            r_left -= 1
-            r_right += 1
-
+            c_left -= 1
+            c_right += 1
         return True
 
     def solveNQueens(self, n):
@@ -46,46 +45,45 @@ class Solution:
         queue_record = [-1 for x in range(n)]
         solutions = []
 
-        c = 0
-        r = 0
+        row = 0
+        column = 0
         has_solution = True
         while has_solution:
 
-            while c < n and c > -1:
+            while row < n and row > -1:
 
-                while r < n or queue_record[c] == -1:
+                while column < n or queue_record[row] == -1:
 
-                    if  r < n and self.is_valid(c, r, queue_record):
-                        board[c][r] = 'Q'
-                        queue_record[c] = r
+                    if  column < n and self.is_valid(row, column, queue_record):
+                        board[row][column] = 'Q'
+                        queue_record[row] = column
                         break
                     else:
-                        r += 1
-                        if r >= n:
-                            c = c - 1
+                        column += 1
+                        if column >= n:
+                            row = row - 1
 
-                            if c < 0:
+                            if row < 0:
                                 has_solution = False
                                 break
 
-                            board[c][queue_record[c]] = '.'
-                            r = queue_record[c] + 1
-                            queue_record[c] = -1
+                            board[row][queue_record[row]] = '.'
+                            column = queue_record[row] + 1
+                            queue_record[row] = -1
 
                 if has_solution:
-                    r = 0
-                    c += 1
+                    column = 0
+                    row += 1
 
             if has_solution:
                 solution = []
                 for e in board:
                     solution.append( "".join(e))
-
                 solutions.append(solution)
-                c = n - 1
-                board[c][queue_record[c]] = '.'
-                r = queue_record[c] + 1
-                queue_record[c] = -1
+                row -= 1
+                board[row][queue_record[row]] = '.'
+                column = queue_record[row] + 1
+                queue_record[row] = -1
 
         return solutions
 
