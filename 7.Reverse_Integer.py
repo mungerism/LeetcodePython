6a9e155cad4d557386146d858d47799e69a3e68f
Utopia <izentopia@gmail.com>
2017-11-27 17:29:50 +0800

    增加不用转换为字符串的方法

--- 7.Reverse_Integer.py
+++ 7.Reverse_Integer.py
@@ -22,7 +22,8 @@ Assume we are dealing with an environment which could only hold integers within 
 
 class Solution:
 
-    def reverse(self, x):
+    # 方法一：先转换为字符串
+    def reverse_str(self, x):
         """
         :type x: int
         :rtype: int
@@ -31,6 +32,22 @@ class Solution:
 
         # 32 位 int 类型的范围，第一位为符号位
         return result if result in range(- 2 ** 31, 2 ** 31 -1) else 0
+
+    # 方法二：不转换为字符串
+    def reverse(self, x):
+
+        symbol = 1 if x >= 0 else -1
+        x *= symbol
+
+        result = 0
+        while x > 0:
+            result = result * 10 + x % 10
+            print(result)
+            x = x // 10
+
+        result *= symbol
+        return result if result in range(- 2 ** 31, 2 ** 31 - 1) else 0
+
 
 if __name__ == '__main__':
     solution = Solution()
