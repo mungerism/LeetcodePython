bb1f5a5c5afe3195e304820ef4b040435d316788
Zentopia <gejunwei110@gmail.com>
2017-11-29 02:24:10 +0800

    完成 4. Median of Two Sorted Arrays

--- 4.Median_of_Two_Sorted_Arrays.py
+++ 4.Median_of_Two_Sorted_Arrays.py
@@ -25,16 +25,37 @@ class Solution:
         :type nums2: List[int]
         :rtype: float
         """
-        merge = []
-
+        merged = []
         i = 0
         j = 0
-        while i < len(nums1) or j < len(nums2):
-            nums1[i] < nums2[j]
+        len_num1 = len(nums1)
+        len_num2 = len(nums2)
+        while i < len_num1 or j < len_num2:
+
+            if i < len_num1 and j < len_num2:
+                if nums1[i] < nums2[j]:
+                    merged.append(nums1[i])
+                    i += 1
+                else:
+                    merged.append(nums2[j])
+                    j += 1
+            else:
+                if i < len_num1:
+                    merged.append(nums1[i])
+                    i += 1
+                else:
+                    merged.append(nums2[j])
+                    j += 1
+
+        len_merged = len(merged)
+        if len_merged % 2 == 0:
+            return (merged[len_merged//2 - 1] + merged[len_merged//2]) / 2
+        else:
+            return merged[(len_merged + 1)//2 - 1]
 
 
 if __name__ == '__main__':
     nums1 = [1, 2]
     nums2 = [3, 4]
     solution = Solution()
-    solution.findMedianSortedArrays(nums1, nums2)
+    print(solution.findMedianSortedArrays(nums1, nums2))
