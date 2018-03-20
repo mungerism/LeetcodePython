a8725c20502a985f9c00c4e83dd6b22cae8c0bdf
Utopia <izentopia@gmail.com>
2018-03-20 11:36:53 +0800

    6. ZigZag Conversion

--- 6. ZigZag Conversion.py
+++ 6. ZigZag Conversion.py
@@ -1,4 +1,6 @@
 '''
+6. ZigZag Conversion
+
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
 P   A   H   N
