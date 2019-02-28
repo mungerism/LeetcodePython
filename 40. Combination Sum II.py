72900fb11426185cd4a5b0324f980c7a7a84ef39
Utopia <izentopia@gmail.com>
2019-02-28 11:37:09 +0800

    40. Combination Sum II

---
+++ 40. Combination Sum II.py
@@ -0,0 +1,74 @@
+"""
+40. Combination Sum II
+
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+"""
+
+
+class Solution:
+    def combinationSum2(self, candidates, target: int):
+        self.candidates = candidates
+        self.candidates.sort()
+        self.target = target
+        self.results = []
+        stack = []
+        self.last_candidate = None
+        self.dfs(target, stack, 0)
+
+        return self.results
+
+    def dfs(self, remainder, stack, index):
+
+        if remainder == 0:
+            self.results.append(stack.copy())
+            return
+
+        if remainder < 0:
+            return
+
+        for i in range(index, len(self.candidates)):
+
+            candidate = self.candidates[i]
+
+            if candidate > remainder:
+                return
+
+            if self.last_candidate == candidate:
+                continue
+
+            remainder -= candidate
+            stack.append(candidate)
+
+            self.dfs(remainder, stack, i + 1)
+            remainder += candidate
+            self.last_candidate = stack.pop()
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
