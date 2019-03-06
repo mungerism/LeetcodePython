3b3b4899b1109ebff20f6deff5e4be8568a2823d
Utopia <izentopia@gmail.com>
2019-03-06 18:49:11 +0800

    215. Kth Largest Element in an Array

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,4 +1,6 @@
 """
+215. Kth Largest Element in an Array
+
 Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 Example 1:
@@ -16,31 +18,31 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
 class Solution(object):
 
-    def quickSort(self, start, end, arr):
+    def quick_sort(self, start, end, arr):
 
-        print(start, end)
-
-        if start - end == 2:
+        if start >= end:
             return
 
         pivot = arr[start]
-        left = start + 1
+        left = start
         right = end
 
-        while arr[right] > pivot and left < right:
-            right -= 1
+        while left < right:
 
-        while arr[left] <= pivot and left < right:
-            left += 1
+            while arr[right] > pivot and left < right:
+                right -= 1
 
-        arr[left], arr[right] = arr[right], arr[left]
-        arr[start], arr[left] = arr[left], arr[start]
+            while arr[left] <= pivot and left < right:
+                left += 1
 
-        self.quickSort(start, right - 1, arr)
-        self.quickSort(right, end, arr)
+            arr[left], arr[right] = arr[right], arr[left]
+
+        arr[start], arr[right] = arr[right], arr[start]
+
+        self.quick_sort(start, right - 1, arr)
+        self.quick_sort(right + 1, end, arr)
 
         return
-
 
 
     def findKthLargest(self, nums, k):
@@ -49,11 +51,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        sort = self.quickSort(0, len(nums)-1, nums)
-        print(nums)
+        self.quick_sort(0, len(nums)-1, nums)
+        return nums[len(nums) - k]
+
 
 if __name__ == '__main__':
 
     # nums = [3,2,3,1,2,4,5,5,6]
-    nums = [3, 2, 1]
-    solution = Solution().findKthLargest(nums ,4)
+    # nums = [2, 1, 3]
+    nums = [2, 1]
+    solution = Solution()
+    print(solution.findKthLargest(nums, 1))
