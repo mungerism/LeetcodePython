c9e242ac125d31acdfd9b43c0b1a2379a068e4af
Zentopia <gejunwei110@gmail.com>
2019-05-14 01:35:38 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,46 @@
+"""
+235. Lowest Common Ancestor of a Binary Search Tree
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+Example 1:
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.recurseTree(root, p, q)
+
+    def recurseTree(self, root, p, q):
+        if root is None:
+            return root
+        if root.val == p.value or root.val == q.value:
+            return root
+
+        if p.val < root.val and q.val < root.val:
+            return self.recurseTree(root.left, p, q)
+
+        if p.val > root.val and q.val > root.val:
+            return self.recurseTree(root.right, p, q)
+
+        return root
