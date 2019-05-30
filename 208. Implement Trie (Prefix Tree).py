26f446448d7bad1dd3a3ca013000c4d31b8d763f
Utopia <izentopia@gmail.com>
2019-05-30 15:30:06 +0800

    69. Sqrt(x) & 208. Implement Trie (Prefix Tree)

---
+++ 208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,77 @@
+"""
+208. Implement Trie (Prefix Tree)
+
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+"""
+
+from collections import defaultdict
+
+
+class TrieNode:
+    def __init__(self):
+        self.dic = defaultdict(TrieNode)
+        self.is_end = False
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        curr = self.root
+        for c in word:
+            curr = curr.dic[c]
+
+        curr.is_end = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr = self.root
+        for c in word:
+            if c not in curr.dic:
+                return False
+            curr = curr.dic[c]
+
+        return curr.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        curr = self.root
+        for c in prefix:
+            if c not in curr.dic:
+                return False
+            curr = curr.dic[c]
+
+        return True
+
+    # Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
