9e85efc37d6c378753b614f92828febfd9c7c937
Zentopia <gejunwei110@gmail.com>
2019-07-04 23:02:57 +0800

    217. Contains Duplicate

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,4 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums) -> bool:
+        dic = {}
+        for num in nums:
+            if dic.get(num) is None:
+                dic[num] = num
+            else:
+                return True
+        return False
 
 
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4]
+    solution = Solution()
+    print(solution.containsDuplicate(nums))
+
+    nums1 = [1, 2, 2, 3]
+    print(solution.containsDuplicate(nums1))
