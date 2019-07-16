723597e67bad5ed55ca16a7cfd023bb147a8ab2a
Zentopia <gejunwei110@gmail.com>
2019-07-16 23:47:49 +0800

    155. Min Stack

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -4,18 +4,35 @@ class MinStack:
         """
         initialize your data structure here.
         """
+        self.stack = []
+        self.min_stack = []
 
     def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or self.min_stack[-1] >= x:
+            self.min_stack.append(x)
 
     def pop(self) -> None:
+        x = self.stack.pop()
+        if self.min_stack[-1] == x:
+            self.min_stack.pop()
 
     def top(self) -> int:
+        return self.stack[-1]
 
     def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+if __name__ == '__main__':
 
 # Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+obj.pop()
+param_3 = obj.top()
+print(param_3)
+param_4 = obj.getMin()
+print(param_4)
