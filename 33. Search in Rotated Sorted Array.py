cc29f1ed5a7c2305733854bb9f37ac7b30c6ae2a
Zentopia <gejunwei110@gmail.com>
2019-08-10 15:38:28 +0800

    33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,18 +1,47 @@
 class Solution:
     def search(self, nums, target: int) -> int:
+
+        if not nums:
+            return -1
+
         left = 0
-        right = len(nums)
+        right = len(nums) - 1
         mid = (left + right) // 2
 
-        while target != nums[mid]:
-            if nums[left] > nums[mid]:
-                mid = (mid - 1 + left) // 2
+        if nums[left] > nums[right]:
+            while nums[mid] < nums[mid + 1]:
+                if nums[left] < nums[mid]:
+                    left = mid
+                    mid = (left + right) // 2
+                else:
+                    right = mid
+                    mid = (left + right) // 2
+
+            rotated_index = mid + 1
+            left = 0
+            right = len(nums) - 1
+
+            if target >= nums[left]:
+                right = rotated_index - 1
+                mid = (left + right) // 2
             else:
-                mid = (mid + 1 + right) // 2
+                left = rotated_index
+                mid = (left + right) // 2
+
+        while target != nums[mid]:
+            if target < nums[mid]:
+                right = mid - 1
+                mid = (left + right) // 2
+            else:
+                left = mid + 1
+                mid = (left + right) // 2
+
+            if left > right:
+                return -1
+
+        return mid
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(5 / 2)
-
-    print(solution.search([4, 5, 6, 7, 0, 1, 2], ))
+    print(solution.search([4, 5, 1, 2, 3], 1))
